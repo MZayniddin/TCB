3735e9d9d68638ec9cf5add231f97bed12f74d50
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-05-10 05:52:47 +0500

    normalized

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-import router from "./routes/index.jsx"
-import "./index.css";
+import reducers from "./reducers"
+import router from "./routes/index.jsx";
+import "./index.scss";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <RouterProvider router={router} />
+    <Provider store={store}>
+        <RouterProvider router={router} />
+    </Provider>
 );
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
     {
         element: <App />,
+        path: "/",
         children: [
-            
-        ]
+            {
+                index: true,
+                element: <Home />,
+            },
+        ],
     },
 ]);
 
